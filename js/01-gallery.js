faa12cde9afab28a68b6c5d6f98298f5160784f9
BartoszZygmunt <it@bartoszzygmunt.pl>
2023-06-07 21:26:44 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,8 +1,8 @@
 import { galleryItems } from "./gallery-items.js";
-//import * as basicLightbox from "basiclightbox";
 
 const galleryElement = document.querySelector(".gallery");
 
+//tworzę znaczniki html
 const galleryItemsMarkup = galleryItems.map((item) => {
   return `
     <li class="gallery__item">
@@ -13,19 +13,21 @@ const galleryItemsMarkup = galleryItems.map((item) => {
   `;
 });
 
+//dodaję do DOM
 galleryElement.innerHTML = galleryItemsMarkup.join("");
-
-const instance = basicLightbox.create(`
-  <div class="modal">
-    <img id="modal-image" src="" alt="Modal Image" />
-  </div>
-`);
 
 galleryElement.addEventListener("click", (event) => {
   event.preventDefault();
   const target = event.target;
   if (target.tagName === "IMG") {
+    const instance = basicLightbox.create(`
+  <div class="modal">
+    <img id="modal-image" src="" alt="Modal Image" />
+  </div>
+`);
     const originalImageSrc = target.parentNode.getAttribute("href");
+    const modalImage = instance.element().querySelector("#modal-image");
+    modalImage.src = originalImageSrc;
     instance.show();
   }
 });
