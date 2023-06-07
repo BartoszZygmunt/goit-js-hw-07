bdf9de378ffd8f19a5ad8eb8dfb62e45e636a032
BartoszZygmunt <it@bartoszzygmunt.pl>
2023-06-08 00:09:33 +0200

    zad2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
 // Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryElement = document.querySelector(".gallery");
+
+//tworzę znaczniki html
+const galleryItemsMarkup = galleryItems.map((item) => {
+  return `
+    <li>
+      <a class="gallery__item" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+      </a>
+    </li>
+  `;
+});
+
+//dodaję do DOM
+galleryElement.innerHTML = galleryItemsMarkup.join("");
+
+//inicjuję galerię
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
